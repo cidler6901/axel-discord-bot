3f32c4d7e7a940786dcc90c781ba69baf14dfbfd
Carson <cidler6901@zohomail.com>
2026-02-02 00:48:20 -0700

    Log TOKEN length on server start

--- index.js
+++ index.js
@@ -36,3 +36,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🌍 Web server running on port ${PORT}`);
 });
+console.log("TOKEN length:", process.env.TOKEN?.length || "NOT FOUND");
